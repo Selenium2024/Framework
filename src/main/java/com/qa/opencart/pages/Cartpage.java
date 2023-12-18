d2b7d1916914a20e400fa67ce38f52cf45192e43
Mahesh <bhallamahesh1990@gmail.com>
2023-12-18 15:38:49 +0530

    updated cart a page

--- src/main/java/com/qa/opencart/pages/Cartpage.java
+++ src/main/java/com/qa/opencart/pages/Cartpage.java
@@ -1,7 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class Cartpage {
-	
-	int i=10;
+
+	int i = 10;
+	private By cart = By.id("id");
+
+	public void getCart() {
+		System.out.println(cart);
+	}
 
 }
